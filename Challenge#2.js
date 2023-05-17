8f68a42b45801e2a44c188d42ffa16fb5529001a
ian <mpebrianalmuhajir@gmail.com>
2023-05-17 09:47:35 +0700

    challenge 2

---
+++ Challenge#2.js
@@ -0,0 +1,26 @@
+function deretKaskus(n){
+    let result = []
+    let end = []
+    
+    for (let i = 1; i <= n; i++) {
+        let number = i * 3;
+        result.push(number)
+    }
+
+    for (let j = 0; j < result.length; j++){
+        if(result[j] % 5 === 0 && result[j] % 6 === 0){
+            end.push("KASKUS")
+        } else if(result[j] % 6 === 0){
+            end.push("KUS")
+        } else if(result[j] % 5 === 0){
+            end.push("KAS")
+        } else {
+            end.push(result[j])
+        }
+        
+    }
+    return end;
+}
+ 
+
+console.log(deretKaskus(10));
