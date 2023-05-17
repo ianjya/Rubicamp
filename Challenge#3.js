19cdae011a1870ec5a2cc971aaf437769684ddbb
ian <mpebrianalmuhajir@gmail.com>
2023-05-17 14:10:11 +0700

    challenge 3

--- Challenge#3.js
+++ Challenge#3.js
@@ -36,5 +36,3 @@ function romawi(number) {
   console.log("13     | XIII       | ", romawi(13));
   console.log("1453   | MCDLIII    | ", romawi(1453));
   console.log("1646   | MDCXLVI    | ", romawi(1646));
-
-  
