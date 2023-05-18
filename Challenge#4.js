f8f02335d1cafd38e527c671b6b635463ebe95c8
ian <mpebrianalmuhajir@gmail.com>
2023-05-18 13:20:57 +0700

    challenge 4

---
+++ Challenge#4.js
@@ -0,0 +1,21 @@
+function checkParam(n){
+    for(let i = 2; i < n; i++){
+        if(n % i === 0 ){
+            return false   
+        } 
+    }
+    return true;
+}
+function indexPrime(param1){
+    let counter = 0;
+    for(var bahan = 2; counter < param1 ; bahan++){
+       if(checkParam(bahan)){
+        counter++
+       }
+    }
+    console.log(bahan - 1);
+}
+
+indexPrime(3);
+
+
