6aeb274d5b7332f48885fb1e665a7dc3e4de9b3e
ian <mpebrianalmuhajir@gmail.com>
2023-05-19 10:41:00 +0700

    String Manipulation

---
+++ Challenge#5.js
@@ -0,0 +1,20 @@
+function stringManipulation(word) {
+    let vokal = ["a", "i", "u", "e", "o"];
+    let firstLetter = word[0].toLowerCase();
+    //
+    const letters = word.split('');
+    const secondLetter = letters.shift();
+
+    
+    if (vokal.includes(firstLetter)) {
+      return word;
+    } else {
+        letters.push(secondLetter);
+        const result =  letters.join('') + "nyo" ;
+
+        return result;
+    }
+    }
+  
+console.log(stringManipulation("bebek"))
+console.log(stringManipulation("ayam"))
