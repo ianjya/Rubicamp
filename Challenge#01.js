a82c15004f3dffc194032f7d77456cdea9c2cfa0
ian <mpebrianalmuhajir@gmail.com>
2023-05-23 17:26:26 +0700

    penambahan

---
+++ Challenge#01.js
@@ -0,0 +1,13 @@
+function sum() {
+    let a = 0;
+    for(let i = 0; i < arguments.length; i++) {
+      a += arguments[i];
+    }
+return a;
+}
+
+console.log(sum(1,2,7))
+console.log(sum(1,4))
+console.log(sum(11))
+console.log(sum(1,2,7))
+console.log(sum(101,2))
